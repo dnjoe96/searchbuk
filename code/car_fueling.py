cbd6b203c489ef14a8b2954d208cb4ff49fe9a49
dnjoe96 <donjoedbest@gmail.com>
2022-02-19 01:04:04 -0800

    Save state

--- code/car_fueling.py
+++ code/car_fueling.py
@@ -1,8 +1,17 @@
 import sys
 
 def compute_min_refills(distance, tank, stops):
-    # write your code here
-    remaining_distance = 400
+    """ The functions attempts to find the minimum refuel stops at designated 
+    fuel stations a vehicle will need to arrive at its destination. And if it
+    doesn't, the function returns -1
+
+    Arg:
+        distance (int): Total distance of trip
+        tank (int): dist a full tank can go
+        stops (list): list of stops, values within 0 to distance
+    """
+
+    # remaining_distance = 400
     # print("distance = ", distance, "tank = ", tank, "stops = ", stops)
 
     count = 0
@@ -10,33 +19,35 @@ def compute_min_refills(distance, tank, stops):
 
     for one in range(len(stops)):
         this_stop = stops[one]
-        # print("this_stop:", stops[one], "last_stop:", last_stop)
+        remaining_distance = distance - this_stop
+        print("this_stop:", stops[one], "last_stop:", last_stop)
         if tank >= (this_stop - last_stop):
 
             final_stop = stops[-1]
             if this_stop != final_stop:
-                next_stop = stops[one+1]
+                next_stop = stops[one + 1]
                 if tank >= (next_stop - last_stop):
                     continue
+                else:
+                    count += 1
+                    last_stop = this_stop
 
-            if this_stop == final_stop:
-                if tank >= remaining_distance:
-                    break
+            if tank >= remaining_distance:
+                return count
 
-            remaining_distance = distance - this_stop
-            last_stop = this_stop
+            # last_stop = this_stop
 
-            count += 1
-            # print("remaining_dist:", remaining_distance)
+            print("remaining_dist:", remaining_distance)
 
         else:
             return -1
 
-    if remaining_distance <= tank:
-        return count
-    return -1
+    #if remaining_distance <= tank:
+    #    return count
+    # return -1
 
 
 if __name__ == '__main__':
-    d, m, _, *stops = map(int, sys.stdin.read().split())
+    #d, m, _, *stops = map(int, sys.stdin.read().split())
+    d, m, stops = 150, 50, [10, 50, 60, 70, 95]
     print(compute_min_refills(d, m, stops))
