61c0753b3365b274598ccedfb11a46f993f5655f
dnjoe96 <donjoedbest@gmail.com>
2022-02-19 10:27:34 +0100

    Remove Non-essential folder

--- code/car_fueling.py
+++ code/car_fueling.py
@@ -1,4 +1,18 @@
+"""
+                **** How The Program works ****
+This program find the minimum number of refuels stops before we arrive
+at a given destination.
+I employed greedy algorithm where I make a stop at the first point at check
+if the fuel capacity is sufficient, before hopping to the next stop. And make
+This check at each stop.
+
+So at every fuel stop I check if we have sufficient fuel capacity to get to
+the next stop. If I do, I keep going, otherwise, I refuel and mark the stop.
+And if at a given stop even with refuel, there is still no sufficient capacity
+to make the next stop, I end the journey and return -1.
+"""
 import sys
+
 
 def compute_min_refills(distance, tank, stops):
     """ The functions attempts to find the minimum refuel stops at designated 
@@ -10,17 +24,13 @@ def compute_min_refills(distance, tank, stops):
         tank (int): dist a full tank can go
         stops (list): list of stops, values within 0 to distance
     """
-
-    # remaining_distance = 400
-    # print("distance = ", distance, "tank = ", tank, "stops = ", stops)
-
     count = 0
     last_stop = 0
 
     for one in range(len(stops)):
         this_stop = stops[one]
         remaining_distance = distance - this_stop
-        print("this_stop:", stops[one], "last_stop:", last_stop)
+
         if tank >= (this_stop - last_stop):
 
             final_stop = stops[-1]
@@ -34,20 +44,15 @@ def compute_min_refills(distance, tank, stops):
 
             if tank >= remaining_distance:
                 return count
-
-            # last_stop = this_stop
-
-            print("remaining_dist:", remaining_distance)
-
+            # print("remaining_dist:", remaining_distance)
         else:
             return -1
 
-    #if remaining_distance <= tank:
-    #    return count
-    # return -1
+    return -1
 
 
 if __name__ == '__main__':
-    #d, m, _, *stops = map(int, sys.stdin.read().split())
-    d, m, stops = 150, 50, [10, 50, 60, 70, 95]
+    d, m, _, *stops = map(int, sys.stdin.read().split())
+    # d, m, stops = 150, 50, [10, 50, 50, 60, 70, 95, 110]
+    # d, m, stops = 150, 50, []
     print(compute_min_refills(d, m, stops))
